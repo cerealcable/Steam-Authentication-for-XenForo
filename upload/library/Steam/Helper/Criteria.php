bd0375bd634ce1a1474a4b1c99ffb46f7e061e00
unknown <cuatthefinish@gmail.com>
2012-04-27 04:34:30 -0400

    Version 1.0.7b

---
+++ upload/library/Steam/Helper/Criteria.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ *      This file is part of Steam Authentication for XenForo
+ *
+ *      Written by Morgan Humes <morgan@lanaddict.com>
+ *      Copyright 2012 Morgan Humes
+ *
+ *      Steam Authentication for XenForo is free software: you can redistribute
+ *      it and/or modify it under the terms of the GNU General Public License
+ *      as published by the Free Software Foundation, either version 3 of the
+ *      License, or (at your option) any later version.
+ *
+ *      Steam Authentication for XenForo is distributed in the hope that it
+ *      will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ *      warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *      See the GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with SteamProfile.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class Steam_Helper_Criteria {
+	public static function criteriaUser($rule, array $data, array $user, &$returnValue) {
+		switch($rule) {
+			case 'steam_state':
+				if(XenForo_Visitor::getUserId() != 0) {
+					switch($data['state']) {
+						case 'associated':
+							if($user['steam_auth_id'] > 0) {
+								$returnValue = true;
+							} else {
+								$returnValue = false;
+							}
+							break;
+						case 'deassociated':
+							if($user['steam_auth_id'] <= 0) {
+								$returnValue = true;
+							} else {
+								$returnValue = false;
+							}
+							break;
+						default:
+							$returnValue = false;
+							break;
+					}
+				}
+				break;
+			case 'steam_game':
+				if(array_key_exists('steam_auth_id', $user) && $user['steam_auth_id'] > 0) {
+					// check if game is in users games table
+					$games = implode(",", $data['games']);
+					$db = XenForo_Application::get('db');
+					$results = $db->fetchAll("SELECT COUNT(*) AS count FROM xf_user_steam_games WHERE user_id = {$user['user_id']} AND game_id IN ($games);");
+					foreach($results as $row) {
+						if($row['count'] > 0) {
+							$returnValue = true;
+						} else {
+							$returnValue = false;
+						}
+						break;
+					}
+				}
+				break;
+			case 'steam_not_game':
+				if(array_key_exists('steam_auth_id', $user) && $user['steam_auth_id'] > 0) {
+					// check if game is NOT in users games table
+					$games = implode(",", $data['games']);
+					$db = XenForo_Application::get('db');
+					$results = $db->fetchAll("SELECT COUNT(*) AS count FROM xf_user_steam_games WHERE user_id = {$user['user_id']} AND game_id IN ($games);");
+					foreach($results as $row) {
+						if($row['count'] > 0) {
+							$returnValue = false;
+						} else {
+							$returnValue = true;
+						}
+						break;
+					}
+				}
+				break;
+		}
+	}
+}
+
+?>
---
+++ upload/library/Steam/Listener.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ *      This file is part of Steam Authentication for XenForo
+ *
+ *      Written by Morgan Humes <morgan@lanaddict.com>
+ *      Copyright 2012 Morgan Humes
+ *
+ *      Steam Authentication for XenForo is free software: you can redistribute
+ *      it and/or modify it under the terms of the GNU General Public License
+ *      as published by the Free Software Foundation, either version 3 of the
+ *      License, or (at your option) any later version.
+ *
+ *      Steam Authentication for XenForo is distributed in the hope that it
+ *      will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ *      warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *      See the GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with SteamProfile.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class Steam_Listener {
+	public static function loadClassController($class, &$extend) {
+		switch($class) {
+			case 'XenForo_ControllerPublic_Register':
+				$extend[] = 'Steam_ControllerPublic_Register';
+				break;
+			case 'XenForo_ControllerPublic_Account':
+				$extend[] = 'Steam_ControllerPublic_Account';
+				break;
+			case 'XenForo_ControllerAdmin_Abstract':
+				$extend[] = 'Steam_ControllerAdmin_Steam';
+				break;
+		}
+	}
+
+	public static function templateCreate($templateName, array &$params, XenForo_Template_Abstract $template) {
+		switch($templateName) {
+			case 'PAGE_CONTAINER':
+				$params['eAuth'] = 1;
+				$template->preloadTemplate('steam_login_bar_item');
+				$template->preloadTemplate('steam_navigation_visitor_tab_link');
+				$template->preloadTemplate('steam_account_wrapper_sidebar_settings');
+				$template->preloadTemplate('steam_message_user_info');
+				$template->preloadTemplate('steam_js');
+				$template->preloadTemplate('steam_member_view_info');
+				$template->preloadTemplate('steam_message_content');
+				$template->preloadTemplate('steam_helper_criteria_privs');
+				break;
+		}
+	}
+
+	public static function templateHook($hookName, &$contents, array $hookParams, XenForo_Template_Abstract $template) {
+		switch($hookName) {
+			case 'login_bar_eauth_items':
+				$contents .= $template->create('steam_login_bar_item', $hookParams);
+				break;
+			case 'navigation_visitor_tab_links1':
+				$contents .= $template->create('steam_navigation_visitor_tab_link', $hookParams);
+				break;
+			case 'account_wrapper_sidebar_settings':
+				$contents .= $template->create('steam_account_wrapper_sidebar_settings', $hookParams);
+				break;
+			case 'message_user_info_text':
+				$contents .= $template->create('steam_message_user_info', array_merge($hookParams, $template->getParams()));
+				break;
+			case 'member_view_info_block':
+				$contents .= $template->create('steam_member_view_info', array_merge($hookParams, $template->getParams()));
+				break;
+			case 'page_container_head':
+				$contents .= $template->create('steam_js', $hookParams);
+				break;
+			case 'message_content':
+				$contents = $template->create('steam_message_content', array_merge($hookParams, $template->getParams())) . $contents;
+				break;
+			case 'user_criteria_extra':
+				$s = new Steam_Helper_Steam();
+				$contents .= $template->create('steam_helper_criteria_privs', array_merge($hookParams, $template->getParams(), array_merge($hookParams, $template->getParams(), array("steam_games" => $s->getAvailableGames()))));
+				break;
+		}
+	}
+}
+
+?>
