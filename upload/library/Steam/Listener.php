660d01548d3ec013e042acd62c6ff9e9cf24aeda
Michael Linback Jr. <cuatthefinish@gmail.com>
2013-01-09 22:28:36 -0800

    Release 1.1.3

--- upload/library/Steam/Listener.php
+++ upload/library/Steam/Listener.php
@@ -34,6 +34,13 @@ class Steam_Listener {
 		}
 	}
 	
+    public static function init(XenForo_Dependencies_Abstract $dependencies, array $data)
+    {
+        XenForo_Template_Helper_Core::$helperCallbacks += array(
+            'steamid' => array('Steam_Helper_Steam', 'convertIdToString')
+        );
+    }
+	
 	public static function navtabs(array &$extraTabs, $selectedTabId)
 	{
 		$options = XenForo_Application::get('options');
